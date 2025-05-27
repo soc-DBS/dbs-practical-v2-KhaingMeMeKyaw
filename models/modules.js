f1ab0c314e6f8c8fb9baed97cf1432b0fcb0a604
KhaingMeMeKyaw <MEME.24@ichat.sp.edu.sg>
2025-05-27 10:58:22 +0800

    test

--- models/modules.js
+++ models/modules.js
@@ -1,6 +1,15 @@
 const { query } = require('../database');
 const { EMPTY_RESULT_ERROR, SQL_ERROR_CODE, UNIQUE_VIOLATION_ERROR } = require('../errors');
 
+module.exports.createRaw = function create(code, name, credit) {
+    const sql = `INSERT INTO module (mod_code, mod_name, credit_unit) VALUES ($1, $2, $3)`;
+    return query(sql, [code, name, credit]).catch(function (error) {
+        if (error.code === SQL_ERROR_CODE.UNIQUE_VIOLATION) {
+            throw new UNIQUE_VIOLATION_ERROR(`Module ${code} already exists`);
+        }
+        throw error;
+    });
+};
 
 //Appendix
 
